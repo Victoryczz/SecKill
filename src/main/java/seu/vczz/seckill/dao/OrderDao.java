bf261c7b03cb9490cdfbaecba032ffe4e3a4554f
Victoryczz <vchenzz93@gmail.com>
2018-05-14 11:24:31 +0800

    秒杀功能大框架

---
+++ src/main/java/seu/vczz/seckill/dao/OrderDao.java
@@ -0,0 +1,13 @@
+package seu.vczz.seckill.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * CREATE by vczz on 2018/5/14
+ */
+@Mapper
+public class OrderDao {
+
+
+
+}
--- src/main/java/seu/vczz/seckill/service/impl/OrderServiceImpl.java
+++ src/main/java/seu/vczz/seckill/service/impl/OrderServiceImpl.java
@@ -1,11 +1,13 @@
 package seu.vczz.seckill.service.impl;
 
+import org.springframework.stereotype.Service;
 import seu.vczz.seckill.service.IOrderService;
 
 /**
  * CREATE by vczz on 2018/5/14
  * 订单service
  */
+@Service
 public class OrderServiceImpl implements IOrderService {
 
 
